6bb17973da1151a0d71afc3e9da98d03403134fa
otammato <104728608+otammato@users.noreply.github.com>
2023-08-23 11:39:26 +0100

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,10 +1,38 @@
+// Importing the bunyan library for structured logging.
 const bunyan = require('bunyan');
+
+// Importing the bunyan-logstash-tcp library to send logs to Logstash via TCP.
 const logstashStream = require('bunyan-logstash-tcp').createStream({
-  host: '172.20.0.3',
-  port: 5000,
+  host: '172.20.0.3',   // Specify the hostname or IP of the Logstash server.
+  port: 5000,           // Specify the port where Logstash is listening.
 });
 
-// define default config, but allow overrides from ENV vars
+// Creating the main logger with multiple streams.
+const mainLogger = bunyan.createLogger({
+  name: 'main-log',     // The name of the logger (used to categorize logs).
+  streams: [
+    {
+      // Stream to write logs to a file.
+      path: './logs.log',
+    },
+    {
+      // Stream to output logs to the console.
+      level: 'debug',
+      stream: process.stdout,
+    },
+    {
+      // Stream to send logs to Logstash over TCP.
+      level: 'debug',
+      type: 'raw',
+      stream: logstashStream,
+    },
+  ],
+});
+
+// Creating a child logger for configuration-related logs. This logger will inherit configurations from the main logger.
+const configLogger = mainLogger.child({ source: 'config' });
+
+// Default configuration values.
 let config = {
   APP_DB_HOST: "localhost",
   APP_DB_USER: "root",
@@ -13,34 +41,18 @@ let config = {
   APP_DB_NAME: "COFFEE"
 }
 
-// Creating a logger instance for general logging purposes
-const mainLogger = bunyan.createLogger({
-  name: 'combined-log',
-  streams: [
-    {
-      path: './logs.log', // Log to a file
-    },
-    {
-      level: 'debug',
-      stream: process.stdout, // Log to console
-    },
-    {
-      level: 'debug',
-      type: 'raw',
-      stream: logstashStream, // Log to Logstash
-    },
-  ],
-  level: 'debug',
-});
-
-// Checking each configuration and logging if they are set from ENV or default value
+// Iterating over each key in the configuration to check if they're present in the environment variables.
 Object.keys(config).forEach(key => {
-  if(process.env[key] === undefined) {
-    // Logging messages about configuration sourcing
-    mainLogger.info(`[NOTICE] Value for key '${key}' not found in ENV, using default value. See app/config/config.js`);
+  if(process.env[key] === undefined){
+    // Logging an info message using the configLogger if the environment variable is not defined.
+    configLogger.info(`[NOTICE] Value for key '${key}' not found in ENV, using default value. See app/config/config.js`);
   } else {
-    config[key] = process.env[key]
+    config[key] = process.env[key];
   }
 });
 
-module.exports = { config, mainLogger };
+// Exporting the configuration and mainLogger for use in other modules.
+module.exports = {
+  config,
+  mainLogger
+};
