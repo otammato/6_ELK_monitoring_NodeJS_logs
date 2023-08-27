b313e905ff82399f292c6fcc4f3e5d6223f71c28
otammato <104728608+otammato@users.noreply.github.com>
2023-08-27 11:16:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,44 +55,38 @@ app.use(favicon(__dirname + "/public/img/favicon.ico"));
 
 // APP endpoints
 app.get("/", (req, res) => {
-  appLogger.info("Routes: GET /: Home route which renders a homepage.");
+  appLogger.info("Route call captured: GET /: Home route which renders a homepage.");
   res.render("home", {});
 });
 
 app.get("/suppliers/", (req, res) => {
-  appLogger.info("Routes: GET /suppliers/: Lists all suppliers.");
+  appLogger.info("Route call captured: GET /suppliers/: Lists all suppliers.");
   supplier.findAll(req, res);
 });
 
 app.get("/supplier-add", (req, res) => {
-  appLogger.info("Routes: GET /supplier-add: Displays the form to add a new supplier.");
+  appLogger.info("Route call captured: GET /supplier-add: Displays the form to add a new supplier.");
   res.render("supplier-add", {});
 });
 
 app.post("/supplier-add", (req, res) => {
-  appLogger.info("Routes: POST /supplier-add: Endpoint to add a new supplier.");
+  appLogger.info("Route call captured: POST /supplier-add: Endpoint to add a new supplier.");
   supplier.create(req, res);
 });
 
 app.get("/supplier-update/:id", (req, res) => {
-  appLogger.info(`Routes: GET /supplier-update/${req.params.id}: Displays the form to update a supplier with a specific ID.`);
+  appLogger.info(`Route call captured: GET /supplier-update/${req.params.id}: Displays the form to update a supplier with a specific ID.`);
   supplier.findOne(req, res);
 });
 
 app.post("/supplier-update", (req, res) => {
-  appLogger.info("Routes: POST /supplier-update: Endpoint to update the supplier details.");
+  appLogger.info("Route call captured: POST /supplier-update: Endpoint to update the supplier details.");
   supplier.update(req, res);
 });
 
 app.post("/supplier-remove/:id", (req, res) => {
-  appLogger.info(`Routes: POST /supplier-remove/${req.params.id}: Endpoint to remove a supplier with a specific ID.`);
+  appLogger.info(`Route call captured: POST /supplier-remove/${req.params.id}: Endpoint to remove a supplier with a specific ID.`);
   supplier.remove(req, res);
-});
-
-// handle 404
-app.use(function (req, res, next) {
-  appLogger.debug("error 404 event");
-  res.status(404).render("404", {});
 });
 
 // Handle 404 errors and log them
